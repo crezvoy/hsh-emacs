4cd089cb7f81aaf90dd0001b54ccfc3018291f94
Clément Rezvoy <clement.rezvoy@gmail.com>
2020-07-20 23:35:15 +0200

    fix pk use on new installs, add dependencies

--- h.sh
+++ h.sh
@@ -21,8 +21,8 @@ case "$HSH_ACTION" in
 		    systemctl --user enable emacs
 		    systemctl --user start emacs
 		fi
-		yes | pk install ispell
-		yes | pk install ifrench-gut
-		yes | pk install iamerican-huge
+		yes | "$HSH_ROOT/bin/pk" install ispell
+		yes | "$HSH_ROOT/bin/pk" install ifrench-gut
+		yes | "$HSH_ROOT/bin/pk" install iamerican-huge
 		;;
 esac
